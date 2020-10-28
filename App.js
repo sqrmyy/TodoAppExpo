ef0d5f67a58f720318e9c61719f87f75846da690
sqrmyy <dn41.ra20adtdjw@gmail.com>
2020-10-29 08:27:23 +0900

    add Home

--- App.js
+++ App.js
@@ -9,96 +9,20 @@ import {
   SafeAreaView,
 } from 'react-native';
 import FormInput from './src/components/FormInput';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+
+const Stack = createStackNavigator();
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoValue: '',
-      memoValue: '',
-      todoList: [],
-    };
-  }
-
   render() {
-    const { todoValue, memoValue, todoList } = this.state;
-
     return (
-      <SafeAreaView style={styles.container}>
-        <FormInput
-          label='やること'
-          value={todoValue}
-          placeholder='何かやること'
-          onChangeText={(v) => this.setState({ todoValue: v })}
-        />
-        <FormInput
-          label='メモ'
-          value={memoValue}
-          placeholder='何かメモ'
-          onChangeText={(v) => this.setState({ memoValue: v })}
-        />
-        <Button
-          title='登録'
-          onPress={() => {
-            const newList = todoList.concat({
-              todo: todoValue,
-              memo: memoValue,
-            });
-            this.setState({
-              todoValue: '',
-              memoValue: '',
-              todoList: newList,
-            });
-          }}
-        />
-        <FlatList
-          style={styles.listBox}
-          data={todoList}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.listItem}>
-                <Text>{item.todo}</Text>
-                <Text>{item.memo}</Text>
-              </View>
-            );
-          }}
-          keyExtractor={(item) => item.todo}
-        />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  formGroup: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 16,
-  },
-  formLabel: {
-    paddingRight: 16,
-  },
-  formControl: {
-    height: 40,
-    width: 160,
-    padding: 8,
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-  listItem: {
-    height: 64,
-    width: 200,
-    marginBottom: 16,
-    padding: 16,
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { SafeAreaView, FlatList, View, Text, StyleSheet } from 'react-native';
+
+/* テストデータ */
+const todoList = [
+  { todo: '買い物', memo: '今日の午後６時まで' },
+  { todo: '掃除', memo: '今日は必ずやる' },
+];
+
+export default class Home extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          style={styles.listBox}
+          data={todoList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.listItem}>
+                <Text>{item.todo}</Text>
+                <Text>{item.memo}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.todo}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    height: 64,
+    width: 200,
+    marginTop: 16,
+    padding: 16,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+});
